de17106fd8a5320b6196b1f036ad435f24bf381f
Pushp Raj <pushp277@gmail.com>
2026-07-27 00:18:28 +0900

    feat(auth-service): create user finishes

--- src/main/java/org/sageDelta/auth_service/api/apiImpl/ApiImpl.java
+++ src/main/java/org/sageDelta/auth_service/api/apiImpl/ApiImpl.java
@@ -1,8 +1,30 @@
 package org.sageDelta.auth_service.api.apiImpl;
 
+import jdk.jshell.Snippet;
+import lombok.RequiredArgsConstructor;
 import org.sageDelta.auth_service.api.ApiApi;
+import org.sageDelta.auth_service.model.User;
+import org.sageDelta.auth_service.services.createUserService.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import java.net.URI;
 
 @Controller
+@RequiredArgsConstructor
 public class ApiImpl implements ApiApi {
+
+    private UserService userService;
+
+    @Override
+    public ResponseEntity<Void> createNewUser(URI host, User user){
+        userService.createUser(user);
+
+        ResponseEntity<Void> responseEntity = ResponseEntity
+                .status(HttpStatus.FOUND)
+                .header("Location", "/login")
+                .build();
+
+        return responseEntity;
+    }
 }
---
+++ src/main/java/org/sageDelta/auth_service/services/createUserService/UserService.java
@@ -0,0 +1,95 @@
+package org.sageDelta.auth_service.services.createUserService;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sageDelta.auth_service.entity.ContactDetailsEntity;
+import org.sageDelta.auth_service.entity.UsersEntity;
+import org.sageDelta.auth_service.exceptions.create.UserAlreadyExists;
+import org.sageDelta.auth_service.model.Address;
+import org.sageDelta.auth_service.model.ContactDetails;
+import org.sageDelta.auth_service.model.User;
+import org.sageDelta.auth_service.repositories.ContactDetailsRepository;
+import org.sageDelta.auth_service.repositories.UserRepository;
+import org.sageDelta.auth_service.utils.Utils;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+   private final ContactDetailsRepository contactDetailsRepository;
+   private final UserRepository userRepository;
+
+    public void createUser(User user){
+        Optional<ContactDetails> contactDetailsOptional = Optional.ofNullable(user.getContactDetails());
+        String username = user.getUsername();
+        if(userRepository.existsByUsername(username)){
+            log.error("username: {} already exists", username);
+            throw new UserAlreadyExists("User Already exists");
+        }
+        if(contactDetailsOptional.isPresent()){
+            ContactDetails contactDetails = contactDetailsOptional.get();
+            log.debug("Contact details: {}", contactDetails);
+            Optional<Address> addressOptional = Optional.ofNullable(contactDetails.getAddress());
+
+            String email = contactDetails.getEmail();
+            if(contactDetailsRepository.existsByEmail(email)){
+                log.error("user email: {} already exists", email);
+                throw new UserAlreadyExists("User Already exists");
+            }
+
+            Long phoneNumer = Long.valueOf(Optional.ofNullable(contactDetails.getPhoneNumber()).orElse("0"));
+
+
+           ContactDetailsEntity contactDetailsEntity = ContactDetailsEntity.builder()
+                   .email(email)
+                   .phoneNumber(phoneNumer)
+                   .build();
+
+           if(addressOptional.isPresent()){
+               Address address = addressOptional.get();
+               String line1 = address.getLine1();
+               String line2 = address.getLine2();
+               String city = address.getCity();
+               String prefecture = address.getPrefecture();
+               long postalCode = Optional.ofNullable(address.getPostalCode()).orElse(0);
+
+               contactDetailsEntity = ContactDetailsEntity.builder()
+                       .phoneNumber(phoneNumer)
+                       .email(email)
+                       .level1(line1)
+                       .level2(line2)
+                       .zipcode(postalCode)
+                       .prefecture(prefecture)
+                       .city(city)
+                       .build();
+           }
+
+           long contactId = contactDetailsRepository.save(contactDetailsEntity).getEntityId();
+
+           String firstName = user.getFirstName();
+           String lastName = user.getLastName();
+           String password = user.getPassword();
+           String salt = Utils.generateSalt();
+           String hashedPassword = Utils.getHash(password, salt);
+
+           UsersEntity usersEntity = UsersEntity.builder()
+                   .username(username)
+                   .password(hashedPassword)
+                   .salt(salt)
+                   .firstName(firstName)
+                   .lastName(lastName)
+                   .contactDetails(contactDetailsEntity)
+                   .build();
+
+           userRepository.save(usersEntity);
+
+           log.info("new User created successfully");
+        }
+    }
+
+}
