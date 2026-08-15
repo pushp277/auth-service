cfc7902a1ec63b4f936bb99fca852d3867231883
Pushp Raj <pushp277@gmail.com>
2026-08-15 21:55:17 +0900

    refactor(spring-secruity): updated spring security

--- src/main/java/org/sageDelta/auth_service/config/LoginSecurityConfig.java
+++ src/main/java/org/sageDelta/auth_service/config/LoginSecurityConfig.java
@@ -1,13 +1,18 @@
 package org.sageDelta.auth_service.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.sageDelta.auth_service.entity.UsersEntity;
 import org.sageDelta.auth_service.exceptions.authorize.UserDoesNotExist;
+import org.sageDelta.auth_service.model.LoginValidationError;
 import org.sageDelta.auth_service.repositories.UserRepository;
 import org.sageDelta.auth_service.security.beans.LoginEntryPoint;
 import org.sageDelta.auth_service.properties.SessionProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,12 +21,18 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.ErrorResponse;
+import tools.jackson.databind.ObjectMapper;
+
 import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 @Slf4j
 public class LoginSecurityConfig {
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain loginSecurityFilterChain(HttpSecurity http,
@@ -46,7 +57,16 @@ public class LoginSecurityConfig {
                     ex.authenticationEntryPoint(loginEntryPoint);
                 })
                 .formLogin((form) -> form
-                        .loginProcessingUrl("/api/v1/login"))
+                        .loginProcessingUrl("/api/v1/login")
+                        .failureHandler((request, response, exception) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+                            LoginValidationError errorResponse = new LoginValidationError();
+                            errorResponse.setMessage("username or password is not correct");
+
+                            objectMapper.writeValue(response.getWriter(), errorResponse);
+                        }))
                 .rememberMe(remember -> remember
                         .rememberMeParameter("rememberMe")
                         .key(sessionConfig.rememberMeSecret())
